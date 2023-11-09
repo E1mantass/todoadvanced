d9afb5ff9126a17671e164601777d5e7807f3d93
E1mantass <meimantas9@gmail.com>
2023-11-09 11:03:24 +0200

    First commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+//grupe
+//checkmarksubject input
+//clickable priority
+//duedate input
+//status
+//percent new0 inprogress25-75 complete100
+//modifiedon 
+
+let subjectInputData = document.getElementById('subject-input').value;
+const subjectDateData = document.getElementById('date-input').value;
+
+console.log(subjectInputData);
+console.log(subjectDateData);
